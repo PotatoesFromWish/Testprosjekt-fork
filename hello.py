c16c979139db3e68255e1095dcee349ed47bf7e2
PotatoesFromWish <adrian_halvorsen@icloud.com>
2025-08-21 09:42:34 +0200

    added asking for your name

--- hello.py
+++ hello.py
@@ -1,3 +1,7 @@
 #Oppgave: få terminal til å spørre om navnet og så
 #printe ut en velkomstmelding til dette navnet
 #prøv så å laste opp prosjektet på github
+
+
+name = input("What is your name?")
+print("Hello", name)
